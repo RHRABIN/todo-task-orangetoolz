b8866d2fd8a10dcf7a1f8cb6e8ba48096b1b7521
RHRABIN <rhrabin934602@gmail.com>
2022-12-13 23:59:37 +0600

    TodoGrid component done and filter all todo with status (todo/in-progress/done) and pass the all todo on TodoList component

--- src/components/todos/TodoGrid.jsx
+++ src/components/todos/TodoGrid.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import TodoList from "./TodoList";
 
 const TodoGrid = () => {
-  return <div></div>;
+  const todos = [
+    { id: 1, name: "todo1", status: "to-do" },
+    { id: 2, name: "todo2", status: "to-do" },
+    { id: 3, name: "todo3", status: "to-do" },
+    { id: 4, name: "todo4", status: "to-do" },
+    { id: 5, name: "todo5", status: "to-do" },
+  ];
+  // filters by status
+
+  // todo filter
+  const filterByTodo = (todo) => todo?.status?.toLowerCase() === "to-do";
+
+  // in-progress filter
+  const filterByInProgress = (todo) =>
+    todo?.status?.toLowerCase() === "in-progress";
+
+  // done filter
+  const filterByDone = (todo) => todo?.status?.toLowerCase() === "done";
+
+  // decide what to render base on todo
+  let content = null;
+  if (todos.length === 0) {
+    content = (
+      <>
+        <p className="text-orange-500 text-center mt-6">
+          No todo found! You can add todo
+        </p>
+        <div className="flex flex-grow px-10 mt-4 space-x-6 overflow-auto">
+          <TodoList title="To Do" />
+          <TodoList title="In Progress" />
+          <TodoList title="Done" />
+        </div>
+      </>
+    );
+  } else if (todos.length > 0) {
+    content = (
+      <div className="flex flex-grow px-10 mt-4 space-x-6 overflow-auto">
+        {/* To do list */}
+        <TodoList title="To Do" todo={todos.filter(filterByTodo)} />
+
+        {/* In Progress list */}
+        <TodoList title="To Do" todo={todos.filter(filterByTodo)} />
+
+        {/* done list */}
+        <TodoList title="To Do" todo={todos.filter(filterByTodo)} />
+      </div>
+    );
+  }
+  return <div>{content}</div>;
 };
 
 export default TodoGrid;
