5e924c95e52a2149fd04bdfaddb13c57a364444e
RHRABIN <rhrabin934602@gmail.com>
2022-12-16 11:45:34 +0600

    Avatar image updated

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import logo from "../assests/logo.jpg";
 import { searchText } from "../redux/filters/actions";
+import avatar from "../assests/avatar.png";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -27,10 +28,13 @@ const Header = () => {
 
   return (
     <div>
+      {/* logo */}
       <div className="flex items-center flex-shrink-0 w-full h-16  bg-opacity-75 justify-between flex-col sm:flex-row mb-28 sm:mb-0">
         <div className="flex gap-4 items-center">
           <img src={logo} className={"w-[220px] h-[114px]"} />
         </div>
+
+        {/* search field */}
         <div>
           <input
             className="flex items-center w-[18.75rem] h-10 px-4  text-sm bg-gray-200 rounded-full focus:outline-none focus:ring"
@@ -39,12 +43,11 @@ const Header = () => {
             onChange={(e) => handleUpdateSearch(e.target.value)}
           />
         </div>
+
+        {/* Avatar image */}
         <div className=" gap-2 items-center hidden sm:flex">
-          <buton className="flex items-center justify-center w-8 h-8 ml-auto overflow-hidden rounded-full cursor-pointer">
-            <img
-              src="https://assets.codepen.io/5041378/internal/avatars/users/default.png?fit=crop&format=auto&height=512&version=1600304177&width=512"
-              alt=""
-            />
+          <buton className="flex items-center justify-center w-12 h-12 ml-auto overflow-hidden rounded-full cursor-pointer">
+            <img src={avatar} alt="avatar" />
           </buton>
         </div>
       </div>
